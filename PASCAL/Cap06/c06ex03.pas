e3f3fa26b284701f024b634af1b2b28c3843b60e
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2024-04-21 20:40:11 -0300

    Update c06ex03.pas

--- PASCAL/Cap06/c06ex03.pas
+++ PASCAL/Cap06/c06ex03.pas
@@ -1,33 +1,77 @@
 program c06ex03;
 
+type
+  TAluno = Record
+    Nome: String;
+    Turma: Char;
+    Sala: Integer;
+    Notas: array[1..4] of Real;
+  end;
+
 var
-  NOTAS: array[1..8, 1..4] of Real;
+  ALUNO: array[1..8] of TAluno;
   I, J: Integer;
 
 begin
+  WriteLn('Escola de Computacao XPTO');
+  WriteLn('Cadastro Escolar de Aluno');
+  WriteLn;
+
   for I := 1 to 8 do
     begin
-      WriteLn('Entre as notas do aluno ', I, ':');
+      WriteLn('Aluno: ', I);
+      WriteLn;
+
+      Write('Nome ..............: ');
+      ReadLn(ALUNO[I].Nome);
+
+      Write('Turma .............: ');
+      ReadLn(ALUNO[I].Turma);
+
+      Write('Sala ..............: ');
+      ReadLn(ALUNO[I].Sala);
+      WriteLn;
+
       for J := 1 to 4 do
         begin
-          Write('Nota ', J, ': ');
-          ReadLn(NOTAS[I, J]);
+          Write('Entre a ', J, 'a. nota ..: ');
+          ReadLn(ALUNO[I].Notas[J]);
         end;
-      WriteLn;
-  end;
 
+      WriteLn;
+    end;
+
+  WriteLn('Escola de Computacao XPTO');
+  WriteLn('Cadastro Escolar de Aluno');
   WriteLn;
-  WriteLn('RELATORIO DE NOTAS');
+  WriteLn('DADOS DOS ALUNOS');
+  Write('Aluno ');
+  Write('Nome                           ');
+  Write('Sala ');
+  Write('Turma ');
+  Write('Nota1 ');
+  Write('Nota2 ');
+  Write('Nota3 ');
+  Write('Nota4 ');
   WriteLn;
-  WriteLn('Aluno Nota1 Nota2 Nota3 Nota4');
-  WriteLn('----- ----- ----- ----- -----');
+  Write('----- ');
+  Write('------------------------------ ');
+  Write('---- ');
+  Write('----- ');
+  Write('----- ');
+  Write('----- ');
+  Write('----- ');
+  Write('----- ');
+  WriteLn;
+
   for I := 1 to 8 do
     begin
       Write(I:5, ' ');
+      Write(ALUNO[I].Nome:30, ' ');
+      Write(ALUNO[I].Sala:4, ' ');
+      Write(ALUNO[I].Turma:5, ' ');
       for J := 1 to 4 do
-        begin
-          Write(NOTAS[I, J]:5:1, ' ');
-        end;
+        Write(ALUNO[I].Notas[J]:5:1, ' ');
       WriteLn;
     end;
 
