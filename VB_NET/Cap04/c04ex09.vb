8e84816c63428635f77677c8bd77a7b3592b8494
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-04-20 12:26:25 -0300

    Update c04ex09.vb

--- VB_NET/Cap04/c04ex09.vb
+++ VB_NET/Cap04/c04ex09.vb
@@ -5,27 +5,27 @@ Imports System.Console
 
 Module Program
     Sub Main(args As String())
-        Dim prova1 As Integer
-        Dim prova2 As Integer
+        Dim p1 As Integer
+        Dim p2 As Integer
 
         WriteLine("Classificacao esportiva")
         WriteLine()
         WriteLine("Entre '1' se atleta pontuou na 1a prova")
         WriteLine("Entre '1' se atleta pontuou na 2a prova")
-        WriteLine("Qualquer outro valor se nao pontuou nas provas")
-        WriteLine()
+        WriteLine("Qualquer outro valor se nao pontuou nas pro-vas")
+       WriteLine()
 
         Write("Prova 1: ")
-        prova1 = Integer.Parse(ReadLine())
+        p1 = Integer.Parse(ReadLine())
 
         Write("Prova 2: ")
-        prova2 = Integer.Parse(ReadLine())
+        p2 = Integer.Parse(ReadLine())
 
-        If (prova1 = 1 Xor prova2 = 1) Then
+        If (p1 = 1 Xor p2 = 1) Then
             WriteLine("Atleta participa da terceira prova.")
         Else
             WriteLine("Atleta nao participa da terceira prova.")
-            If (prova1 = 1 And prova2 = 1) Then
+            If (p1 = 1 And p2 = 1) Then
                 WriteLine("Classificado para a final.")
             Else
                 WriteLine("Desclassificado da competicao.")
