ce2196912b4c3d316111415e1508808474e4000d
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-04-20 12:02:38 -0300

    Update c04ex09.scala

--- SCALA/Cap04/c04ex09.scala
+++ SCALA/Cap04/c04ex09.scala
@@ -4,8 +4,8 @@ import scala.io.StdIn._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    var prova1: Int = 0
-    var prova2: Int = 0
+    var p1: Int = 0
+    var p2: Int = 0
 
     println("Classificacao esportiva\n")
     println("Entre '1' se atleta pontuou na 1a prova")
@@ -13,16 +13,16 @@ object Main {
     println("Qualquer outro valor se nao pontuou nas provas\n")
 
     print("Prova 1: ")
-    prova1 = readInt()
+    p1 = readInt()
 
     print("Prova 2: ")
-    prova2 = readInt()
+    p2 = readInt()
 
-    if (prova1 == 1 ^ prova2 == 1) {
+    if (p1 == 1 ^ p2 == 1) {
       println("Atleta participa da terceira prova.")
     } else {
       println("Atleta nao participa da terceira prova.")
-      if (prova1 == 1 && prova2 == 1) {
+      if (p1 == 1 && p2 == 1) {
         println("Classificado para a final.")
       } else {
         println("Desclassificado da competicao.")
