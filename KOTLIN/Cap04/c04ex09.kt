2aae2e1181926d9014e0d343782e1d4a27929744
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-04-20 11:46:59 -0300

    Update c04ex09.kt

--- KOTLIN/Cap04/c04ex09.kt
+++ KOTLIN/Cap04/c04ex09.kt
@@ -1,8 +1,9 @@
+
 // c04ex09.kt
 
 fun main() {
-    var prova1: Int
-    var prova2: Int
+    var p1: Int
+    var p2: Int
 
     println("Classificacao esportiva\n")
     println("Entre '1' se atleta pontuou na 1a prova")
@@ -10,16 +11,16 @@ fun main() {
     println("Qualquer outro valor se nao pontuou nas provas\n")
 
     print("Prova 1: ")
-    prova1 = readLine()?.toInt() ?: 0
+    p1 = readLine()?.toInt() ?: 0
 
     print("Prova 2: ")
-    prova2 = readLine()?.toInt() ?: 0
+    p2 = readLine()?.toInt() ?: 0
 
-    if (prova1 == 1 && prova2 != 1 || prova1 != 1 && prova2 == 1) {
+    if ((p1 == 1 && !(p2 == 1)) || (!(p1 == 1) && p2 == 1)) {
         println("Atleta participa da terceira prova.")
     } else {
         println("Atleta nao participa da terceira prova.")
-        if (prova1 == 1 && prova2 == 1) {
+        if (p1 == 1 && p2 == 1) {
             println("Classificado para a final.")
         } else {
             println("Desclassificado da competicao.")
@@ -28,4 +29,4 @@ fun main() {
 
     print("\nPressione <Enter> para encerrar... ")
     readLine()
-}
+}    
