fa1156831bf12ae673dcd430fec1aee856d791e8
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-04-20 11:36:20 -0300

    Update c04ex09.jl

--- JULIA/Cap04/c04ex09.jl
+++ JULIA/Cap04/c04ex09.jl
@@ -1,20 +1,27 @@
-# c04ex08.jl
+# c04ex09.jl
 
-println("Escolha o tipo de transporte:")
+println("Classificacao esportiva")
 println()
-println("[M] - Motocicleta")
-println("[S] - Scooter")
-println()
-
-print("==> ")
-transp = uppercase(readline())
-
+println("Entre '1' se atleta pontuou na 1a. prova")
+println("Entre '1' se atleta pontuou na 2a. prova")
+println("Qualquer outro valor se nao pontuou nas provas")
 println()
 
-if (transp == "M") || (transp == "S")
-  println("Transporte valido.")
+print("Prova 1: ")
+p1 = readline()
+
+print("Prova 2: ")
+p2 = readline()
+
+if (p1 == "1") ⊻ (p2 == "1")
+  println("Atleta participa da terceira prova.")
 else
-  println("Transporte invalido.")
+  println("Atleta nao participa da terceira prova.")
+  if (p1 == "1") && (p2 == "1")
+    println("Classificado para a final.")
+  else
+    println("Desclassificado da competicao.")
+  end
 end
 
 println()
