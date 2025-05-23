3f6894f5167f1bb6536d1ef111c6ef6b7a5a7f77
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-05-23 12:51:27 -0300

    Update c08ex09.py

--- PYTHON/Cap08/c08ex09.py
+++ PYTHON/Cap08/c08ex09.py
@@ -3,9 +3,9 @@
 def fibonacci(n):
   if (n == 0):
     return 0
-  elif (n == 1):
+  if (n == 1):
     return 1
-  else:
+  if (n >= 2):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 for i in range(15):
