17fcdf4d050ef0c23f07c89b980df93bd7f58731
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-05-23 08:14:16 -0300

    Update c08ex09.d

--- D/Cap08/c08ex09.d
+++ D/Cap08/c08ex09.d
@@ -9,7 +9,10 @@ int Fibonacci(int N) {
   if (N == 1) {
     return 1;
   }
-  return Fibonacci(N - 1) + Fibonacci(N - 2);
+  if (N >= 2) {
+    return Fibonacci(N - 1) + Fibonacci(N - 2);
+  }
+  return 0;
 }
 
 void main() {
