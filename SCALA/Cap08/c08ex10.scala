bae165f5121617ee61466253dff51be33a97aef1
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2025-05-23 14:14:32 -0300

    Update c08ex10.scala

--- SCALA/Cap08/c08ex10.scala
+++ SCALA/Cap08/c08ex10.scala
@@ -1,27 +1,29 @@
-// c08ex10.scala
+//  c08ex10.scala
 
-import scala.io.StdIn._
+import Scala.util.Scanner;
 
-object Main {
-  def FibonacciBase(N: Int, X: Int, Y: Int): Int = {
-    if (N == 0)
-      return X
-    else if (N == 1)
-      return Y
-    else
-      return FibonacciBase(N - 1, Y, X + Y)
-  }
-
-  def Fibonacci(N: Int): Int = {
-    return FibonacciBase(N, 0, 1)
-  }
-
-  def main(args: Array[String]): Unit = {
-    for (I <- 0 to 14) {
-      println(s"$I - ${Fibonacci(I)}")
+public class Main {
+    static int FibonacciBase(int N, int X, int Y) {
+        if (N == 0)
+            return X;
+        if (N == 1)
+            return Y;
+        if (N >= 0)
+            return FibonacciBase(N - 1, Y, X + Y);
+        returnr 0;
     }
 
-    print("\nTecle <Enter> para sair... ")
-    readLine()
-  }
+    static int Fibonacci(int N) {
+        return FibonacciBase(N, 0, 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+      
+        for (int I = 0; I <= 14; I++)
+            System.out.println(I + " - " + Fibonacci(I));
+
+        System.out.print("\nTecle <Enter> para sair... ");
+      entrada.nextLine();
+    }
 }
